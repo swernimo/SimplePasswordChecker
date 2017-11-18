7a51fcc1534d7841b4540841a0014c269499515b
Sean Wernimont <swernimont@paylocity.com>
2017-11-18 11:42:36 -0500

    added scratpad and auth module. added function to auth module to generate SHA512 hash of a password.

---
+++ SimplePasswordChecker/SimplePasswordChecker/Auth.fs
@@ -0,0 +1,8 @@
+﻿module Auth
+
+open System.Security.Cryptography
+open System.Text
+
+let getPasswordHash (password:string) = 
+    use sha512 = SHA512.Create()
+    ASCIIEncoding.UTF8.GetBytes password |> sha512.ComputeHash |>Seq.fold (fun str byte -> str + byte.ToString("x2")) ""
---
+++ SimplePasswordChecker/SimplePasswordChecker/ScratchPad.fsx
@@ -0,0 +1,13 @@
+﻿open System.Security.Cryptography
+open System.Text
+
+let getPasswordHash (password:string) = 
+    use sha512 = SHA512.Create()
+    ASCIIEncoding.UTF8.GetBytes password |> sha512.ComputeHash |>Seq.fold (fun str byte -> str + byte.ToString("x2")) ""
+    
+    //let bytes = ASCIIEncoding.UTF8.GetBytes password
+    //let hash = sha512.ComputeHash bytes
+    //hash |> Seq.fold (fun str byte -> str + byte.ToString("x2")) ""
+    
+
+getPasswordHash "Abc123"
