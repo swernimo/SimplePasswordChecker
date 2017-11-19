873f91fd425e04ef2fed8c42116da5bfc16b28a1
Sean Wernimont <swernimont@paylocity.com>
2017-11-19 09:07:59 -0500

    still trying to ignore script file.

--- SimplePasswordChecker/SimplePasswordChecker/DataAccess.fs
+++ SimplePasswordChecker/SimplePasswordChecker/DataAccess.fs
@@ -3,7 +3,7 @@
 open FSharp.Data
 
 [<Literal>]
-let connectionString = @"some connection string"
+let connectionString = @"Server=tcp:fsharp-sql.database.windows.net,1433;Initial Catalog=Users;Persist Security Info=False;User ID=swernimo;Password=xPh7de6g;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 
 let getPreviousPasswords user =
     use cmd = new SqlCommandProvider<"select Id, Username, Password from dbo.[User] where Username = @username", connectionString, ResultType.Records>(connectionString)
--- SimplePasswordChecker/SimplePasswordChecker/ScratchPad.fsx
+++ SimplePasswordChecker/SimplePasswordChecker/ScratchPad.fsx
@@ -1,1 +1,24 @@
-﻿
+﻿open System.Security.Cryptography
+open System.Text
+
+let getPasswordHash (password:string) = 
+    use sha512 = SHA512.Create()
+    ASCIIEncoding.UTF8.GetBytes password |> sha512.ComputeHash |>Seq.fold (fun hash byte -> hash + byte.ToString("x2")) ""
+    
+    //let bytes = ASCIIEncoding.UTF8.GetBytes password
+    //let hash = sha512.ComputeHash bytes
+    //hash |> Seq.fold (fun str byte -> str + byte.ToString("x2")) ""
+
+//let getPasswordHash (password: string) =
+//    use sha512 = SHA512.Create()
+//    let bytes = ASCIIEncoding.UTF8.GetBytes password
+//    let hashAsBytes = sha512.ComputeHash bytes
+//    let hash = hashAsBytes |> Seq.fold (fun str byte -> str + byte.ToString("x2")) ""
+//    hash
+    //let mutable hashString = ""
+    //for b in hashAsBytes do
+    //    hashString <- hashString + b.ToString("x2")
+    //hashString
+
+getPasswordHash "Abc123"
+//getPasswordHashCSharp "Abc123"
